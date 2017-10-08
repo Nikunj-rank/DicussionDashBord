734a0a692dd96bbc2fc2c631fd57d52a03b4bec1
Nikunj Rank <contact@sout.co.in>
2017-10-08 23:18:59 +0530

    added new changes for dashboard

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,8 +4,8 @@ var usernamePage = document.querySelector('#username-page');
 var chatPage = document.querySelector('#chat-page');
 var usernameForm = document.querySelector('#usernameForm');
 // var messageForm = document.querySelector('#messageForm');
- var messageInput = document.querySelector('#message');
- var messageArea = document.querySelector('#messageArea');
+var messageInput = document.querySelector('#message');
+var messageArea = document.querySelector('#messageArea');
 // var connectingElement = document.querySelector('.connecting');
 
 var stompClient = null;
@@ -16,10 +16,61 @@ var colors = [
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
 
+var oCommonObject = {
+    /* Wrapper function to call specified method of the service with specified parameters, success and failure functions */
+    callService: function (sWebMethodName, oParameters, onSuccess, onFailure, onBeforeSend, oParam) {
+        if (typeof (oParameters) === 'string') {
+            sData = oParameters;
+        } else {
+            sData = JSON.stringify(oParameters);
+        }
+        var sWebMethodUrl = commonConstants.sServiceLocation + "/" + sWebMethodName
+            , sData;
+
+        oServiceGlobal.oAjaxRequest = $.ajax({
+            type: "POST",
+            url: sWebMethodUrl,
+            data: sData,
+            contentType: "application/json;",
+
+            success: function (result) {
+                if ("function" === typeof (onSuccess)) {
+                    if (oParam) {
+                        var oFinalResult = { "Result": result, "oParam": oParam };
+                        onSuccess(oFinalResult);
+                    } else {
+                        onSuccess(result);
+                    }
+                }
+            },
+            beforeSend: function (result) {
+                if ("function" === typeof (onBeforeSend)) {
+                    if (oParam) {
+                        var oFinalResult = { "Result": result, "oParam": oParam };
+                        onBeforeSend(oFinalResult);
+                    } else {
+                        onBeforeSend(result);
+                    }
+                }
+            },
+            error: function (result) {
+                if ("function" === typeof (onFailure)) {
+                    if (oParam) {
+                        var oFinalResult = { "Result": result, "oParam": oParam };
+                        onFailure(oFinalResult);
+                    } else {
+                        onFailure(result);
+                    }
+                }
+            }
+        });
+    }
+};
+
 function connect(event) {
     username = document.querySelector('#name').value.trim();
 
-    if(username) {
+    if (username) {
 
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
@@ -40,7 +91,7 @@ function onConnected() {
     // Tell your username to the server
     stompClient.send("/app/chat.addUser",
         {},
-        JSON.stringify({sender: username, type: 'JOIN'})
+        JSON.stringify({ sender: username, type: 'JOIN' })
     )
 
     // connectingElement.classList.add('hidden');
@@ -58,7 +109,7 @@ function onError(error) {
 function sendMessage(event) {
     var messageContent = messageInput.value.trim();
 
-    if(messageContent && stompClient) {
+    if (messageContent && stompClient) {
         var chatMessage = {
             userName: username,
             comment: messageInput.value
@@ -84,20 +135,20 @@ function onMessageReceived(payload) {
     //     messageElement.classList.add('event-message');
     //     message.content = message.sender + ' left!';
     // } else {
-        var MessageBox = "<li class='right clearfix'>"+
-        "<span class='chat-img pull-right'>"+
-        "<img src='http://placehold.it/50/FA6F57/fff&text=RGB' alt='User Avatar' class='img-circle' />"+
-        "</span>"+
-        "<div class='chat-body clearfix'>"+
-        "<div class='header'>"+
-        "<small class=' text-muted'><span class='glyphicon glyphicon-time'></span>15 mins ago</small>"+
-        "<strong class='pull-right primary-font'>"+message.userName+"</strong>"+
-        "</div>"+
-        "<p>"+message.comment+
-        "</p>"+
-        "</div>"+
+    var MessageBox = "<li class='right clearfix'>" +
+        "<span class='chat-img pull-right'>" +
+        "<img src='http://placehold.it/50/FA6F57/fff&text=RGB' alt='User Avatar' class='img-circle' />" +
+        "</span>" +
+        "<div class='chat-body clearfix'>" +
+        "<div class='header'>" +
+        "<small class=' text-muted'><span class='glyphicon glyphicon-time'></span>15 mins ago</small>" +
+        "<strong class='pull-right primary-font'>" + message.userName + "</strong>" +
+        "</div>" +
+        "<p>" + message.comment +
+        "</p>" +
+        "</div>" +
         "</li>";
-        $('#messageArea').append(MessageBox);
+    $('#messageArea').append(MessageBox);
 
     //     messageElement.classList.add('chat-message');
 
@@ -134,26 +185,50 @@ function getAvatarColor(messageSender) {
     var index = Math.abs(hash % colors.length);
     return colors[index];
 }
+function getPostSuccess(data){
+var tableRow = "<tr>"+
+                    "<td><img src='http://placehold.it/140x100' alt=''></td>"+
+                    "<td>"+
+                        "<h4>"+
+                            "<a href='#'>{{topicName}}</a> "+
+                            "<span class='time'><sup>9 hours ago</sup></span>"+
+                        "</h4>"+
+                        "<p>{{topicDesc}}</p>"+
+                    "</td>"+
+                    "<td>{{topicLike}}</td>"+
+                    "<td>{{topicDisLike}}</td>"+
+                "</tr>";
+              $.each(data,function(key,value){
+                  var row = tableRow.replace("{{topicName}}",value.subject).replace("{{topicDesc}}",value.desc).replace("{{topicLike}}",value.listOfUserLiked.Length).replace("{{topicDisLike}}",value.listOfUserDisLiked.Length);
+                   $("#postTable").append(row);
+            });
+}
 
-$(document).ready(function(){
-//usernameForm.addEventListener('submit', connect, true);
+$(document).ready(function () {
+    //usernameForm.addEventListener('submit', connect, true);
+    if (0 !== $("#dashboardPage").length) {
 
-$("#btn-send-comment").click(function(){
-    sendMessage();
+        oCommonObject.callService("get/stock/all", localStorage.getItem('userName'), getPostSuccess, getPostFailure, null, null);
+    } else if (0 !== $("#indexPage").length) {
+
+    }
+
+    $("#btn-send-comment").click(function (event) {
+        sendMessage(event);
+    });
+
+    $("#login-submit").on('click', function (event) {
+        doLogin(event);
+    });
+
+    // messageForm.addEventListener('submit', sendMessage, true)
 });
 
-$("#login-submit").on('click',function(event){
-    doLogin(event);
-});
-
-// messageForm.addEventListener('submit', sendMessage, true)
-});
-
-function doLogin(event){
-    localStorage.setItem("userName",$("#userNameId").val());
-    var oLoginDetail = {"username":$("#userNameId").val(),"password" : $("#passwordId").val()};
+function doLogin(event) {
+    localStorage.setItem("userName", $("#userNameId").val());
+    var oLoginDetail = { "username": $("#userNameId").val(), "password": $("#passwordId").val() };
     console.log(oLoginDetail);
-    post("login",oLoginDetail);
+    post("login", oLoginDetail);
 }
 
 function post(path, params, method) {
