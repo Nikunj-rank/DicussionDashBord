c472ec2c533e3054c099bc08c23fbd1c32002499
sout-nikunjr <contact@sout.co.in>
2017-10-08 22:57:36 +0530

    controller change

---
+++ src/main/java/com/example/websocketdemo/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.example.websocketdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+
+@SuppressWarnings("SpringJavaAutowiringInspection")
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+//    @Autowired
+//    DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,
+                        "/login",
+                        "/**/*.css",
+                        "/**/*.js").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/index")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("mv").password("1").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("kt").password("password").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("dm").password("password").roles("USER");
+    }
+
+    // below code is working we will switch to that once we will provide support from front end
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication().dataSource(dataSource)
+//                .usersByUsernameQuery(
+//                        "select username,password, enabled from user_model where username=?")
+//          .authoritiesByUsernameQuery(
+//                "select username, role from user_roles where username=?");
+//    }
+
+
+}
